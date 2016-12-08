09f5e9d3059eb8e7692a19536dc3b56ab6df1738
Adam Jedro <jedroadam@gmail.com>
2016-12-08 21:42:33 +0100

    Implemented getextradata from VBoxManage

--- extra.go
+++ extra.go
@@ -1,5 +1,10 @@
 package virtualbox
 
+import (
+	"fmt"
+	"strings"
+)
+
 // SetExtra sets extra data. Name could be "global"|<uuid>|<vmname>
 func SetExtra(name, key, val string) error {
 	return vbm("setextradata", name, key, val)
@@ -8,4 +13,18 @@ func SetExtra(name, key, val string) error {
 // DelExtraData deletes extra data. Name could be "global"|<uuid>|<vmname>
 func DelExtra(name, key string) error {
 	return vbm("setextradata", name, key)
+}
+
+// GetExtraData gets extra data. Name could be "global"|<uuid>|<vmname>
+func GetExtraData(name, key string) (string, error) {
+	out, err := vbmOut("getextradata", name, key)
+	if err != nil {
+		return "", err
+	}
+
+	if strings.HasPrefix(out, "Value: ") {
+		return out[len("Value: "):], nil
+	}
+
+	return "", fmt.Errorf("Cannot get extra data for machine %s for key %s", name, key)
 }
